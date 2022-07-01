6ef53d1c748818e95b23005ea2cdd8057a369eaf
kleora86 <106878059+kleora86@users.noreply.github.com>
2022-07-01 13:28:06 +0300

    в

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
-  
+   if (str===0){return "Zero";} else
+  if (str===1){return "One";} else
+  if (str===2){return "Two";} else
+  if (str===3){return "Three";}else
+  if (str===4){return "Four";} else
+  if (str===5){return "Five";} else
+  if (str===6){return "Six";}else
+  if (str===7){return "Seven";} else
+  if (str===8){return "Eight";} else
+  if (str===9){return "Nine";}else
+  if (str===10){return "Ten";} else
+  if (str===11){return "Eleven";}else
+  if (str===12){return "Twelwe";} else
+  if (str===13){return "Thrity";} else
+  if (str===14){return "Forty";}else
+  if (str===15){return "Fifty";} else
+  if (str===16){return "Sixty";} else
+  if (str===17){return "Seventy";}else
+  if (str===18){return "Eitheen";} else
+  if (str===19){return "Nineteen";} else
+  if (str===20){return "Twelwe";}else
+   return "FAL-SE";
 }
