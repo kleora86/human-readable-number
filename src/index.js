d3dd1ceea132ffc5ab02ebc9a7c64e3ab840bcb6
kleora86 <Random_les@mail.ru>
2022-07-02 20:31:08 +0300

    hh

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
 module.exports = function toReadable (str) {
-    if (str===0){return "Zero";} else
-  if (str===1){return "One";} else
-  if (str===2){return "Two";} else
-  if (str===3){return "Three";}else
-  if (str===4){return "Four";} else
-  if (str===5){return "Five";} else
-  if (str===6){return "Six";}else
-  if (str===7){return "Seven";} else
-  if (str===8){return "Eight";} else
-  if (str===9){return "Nine";}else
-  if (str===10){return "Ten";} else
-  if (str===11){return "Eleven";}else
-  if (str===12){return "Twelwe";} else
-  if (str===13){return "Thrity";} else
-  if (str===14){return "Forty";}else
-  if (str===15){return "Fifty";} else
-  if (str===16){return "Sixty";} else
-  if (str===17){return "Seventy";}else
-  if (str===18){return "Eitheen";} else
-  if (str===19){return "Nineteen";} else
-  if (str===20){return "Twelwe";}else
-   return "FAL-SE";
+  const first = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+  const tens = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+  const mad = ['', 'thousand', 'million', 'billion', 'trillion'];
+  let word = '';
 
+  for (let i = 0; i < mad.length; i++) {
+    let tempNumber = number%(100*Math.pow(1000,i));
+    if (Math.floor(tempNumber/Math.pow(1000,i)) !== 0) {
+      if (Math.floor(tempNumber/Math.pow(1000,i)) < 20) {
+        word = first[Math.floor(tempNumber/Math.pow(1000,i))] + mad[i] + ' ' + word;
+      } else {
+        word = tens[Math.floor(tempNumber/(10*Math.pow(1000,i)))] + '-' + first[Math.floor(tempNumber/Math.pow(1000,i))%10] + mad[i] + ' ' + word;
+      }
+    }
+
+    tempNumber = number%(Math.pow(1000,i+1));
+    if (Math.floor(tempNumber/(100*Math.pow(1000,i))) !== 0) word = first[Math.floor(tempNumber/(100*Math.pow(1000,i)))] + 'hunderd ' + word;
+  }
+    return word;
 }
